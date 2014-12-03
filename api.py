bbb5bc405af75b34208951612ab672d496f36f52
Binbin FU <fubinbinole@gmail.com>
2014-12-03 10:15:30 -0600

    some api&ui

---
+++ api.py
@@ -0,0 +1,346 @@
+import cgi
+import urllib
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.api import images
+import webapp2
+import json
+import base64
+from datetime import *
+
+# Images under an Activity, its parent is an activity
+class Imag(ndb.Model):
+    url = ndb.StringProperty()
+    blob_key = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add = True)
+
+# Greeting to person
+class Greeting_person(ndb.Model):
+    author = ndb.StringProperty()
+    content = ndb.StringProperty(indexed = False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class Activity_comment(ndb.Model):
+    author = ndb.StringProperty()
+    content = ndb.StringProperty(indexed = False)
+    date = ndb.DateTimeProperty(auto_now_add = True)
+# Activity
+class Activity(ndb.Model):
+    # Content of the Activity
+    title = ndb.StringProperty()#Activity title
+    start_date = ndb.DateTimeProperty()#Start date
+    end_date = ndb.DateTimeProperty()#End date
+    neighborhood = ndb.StringProperty()
+    zipcode = ndb.StringProperty()
+    address = ndb.StringProperty()#Location of the activity
+    latitude = ndb.StringProperty()#For google map
+    longitude = ndb.StringProperty()#For google map
+    host = ndb.StringProperty()#The name of the host
+    tag = ndb.StringProperty()#The type of the activity
+    details = ndb.StringProperty() # The details of the activity
+    # others
+    like_number = ndb.IntegerProperty()
+    take_number = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty(auto_now_add = True)#The create date
+    cover = ndb.StringProperty()#Blobstore key
+
+class Webusers(ndb.Model):#Each Webusers xx.key.id() is the user
+    nickname = ndb.StringProperty()
+    gender = ndb.StringProperty()
+    birthday = ndb.DateTimeProperty()
+    my_activity = ndb.StringProperty(repeated = True) #The id of the activity I post
+    like_activity = ndb.StringProperty(repeated = True)#The id of the activity I like
+    #want_take_activity = ndb.StringProperty(repeated = True)#The id of the activity I want to take
+    take_activity = ndb.StringProperty(repeated = True)#The id of the activity I have taken
+    photo = ndb.StringProperty()
+    subscribe = ndb.StringProperty(repeated = True)
+    interest = ndb.StringProperty(repeated = True)
+    introduce = ndb.StringProperty() 
+
+#Post activity upload to blobstore and database
+class Upload_api(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+	print "hehe"
+	new_host = self.request.get('user')
+	user = ndb.Key(Webusers, new_host).get()
+	if user:
+	    new_tag = self.request.get('tag')
+	    new_title = self.request.get('title')
+	    new_start_date = self.request.get('start_date')
+	    new_end_date = self.request.get('end_date')
+	    new_neighborhood = self.request.get('neighborhoods')
+ 	    new_zipcode = self.request.get('zipcode')
+	    new_address = self.request.get('address')
+	    new_details= self.request.get('details')
+	    new_latitude = self.request.get('latitude')
+	    new_longitude = self.request.get('longitude')
+	    new_cover = self.get_uploads('cover')
+	    if new_cover:
+	        blob_info = new_cover[0]
+	        blob_key = str(blob_info.key())
+	    else:
+	        blob_key = ''
+	    print "hello"
+	    print blob_key
+	    new_start_date = datetime(int(new_start_date[0:4]), int(new_start_date[5:7]), int(new_start_date[8:10]), int(new_start_date[11:13]), int(new_start_date[14:16]))
+	    new_end_date = datetime(int(new_end_date[0:4]), int(new_end_date[5:7]), int(new_end_date[8:10]), int(new_end_date[11:13]), int(new_end_date[14:16]))
+	    #activity	
+	    activity = Activity(title = new_title, tag = new_tag, neighborhood = new_neighborhood, zipcode = new_zipcode, address = new_address, latitude = new_latitude, longitude = new_longitude, host = new_host, cover = blob_key, start_date = new_start_date, end_date = new_end_date, details = new_details, like_number = 0, take_number = 0)
+	    activity_key = activity.put()
+	    #user
+	    user.my_activity.append(str(activity_key.id()))
+	    user.put()
+	    self.redirect('/post')
+	    
+
+class Profile_api(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+	user_ = self.request.get('user')
+	user_query = ndb.Key(Webusers, str(user_)).get()
+	if not user_query:
+	    user_query = Webusers(id=str(user_))
+	user_query.nickname = self.request.get('nick')
+	user_query.gender = self.request.get('gender')
+	birthday_ = self.request.get('birthday')
+	user_query.birthday = datetime(int(birthday_[0:4]),int(birthday_[5:7]),int(birthday_[8:10]))
+	user_query.introduce = self.request.get('introduce')
+	#interest
+	if user_query.interest:
+	    user_query.interest = []
+	for item in self.request.get_all('interest'):
+	    user_query.interest.append(str(item))
+	photo = self.get_uploads('photo')
+	if photo:
+	    if user_query.photo:
+                blobstore.delete(urllib.unquote(user_query.photo))
+	    blob_info = photo[0]
+	    user_query.photo = str(blob_info.key())
+	user_query.put()
+	self.redirect('/my_city') 
+
+class Person_api(webapp2.RequestHandler):
+    def post(self):
+	requests = json.loads(self.request.body)
+        user_id = requests['user_id']
+	user = ndb.Key(Webusers, str(user_id)).get()
+	users = Webusers.query().fetch()
+	i = 0
+	for each in users:
+	    if user_id in each.subscribe:
+		i = i + 1
+	#greeting
+	greetings_author = list()
+	greetings_content = list()
+	greetings = Greeting_person.query(ancestor=ndb.Key(Webusers, str(user_id))).order(Greeting_person.date).fetch()
+	for greeting in greetings:
+	    greetings_author.append(greeting.author)
+	    greetings_content.append(greeting.content)
+	responses = {
+	    'taken': user.take_activity,
+	    'like': user.like_activity,
+	    'post': user.my_activity,
+	    'nick': user.nickname,
+	    'introduce':user.introduce,
+	    'interest':user.interest,
+	    'photo':user.photo,
+	    'sub_me_number': i,
+	    'gender':user.gender,
+	    'greetings_author':greetings_author,
+	    'greetings_content':greetings_content
+	}
+        self.response.headers['Content-Type'] = "application/json"
+        self.response.headers['Accept'] = "text/plain"
+        self.response.write(json.dumps(responses))
+
+class Greeting_person_api(webapp2.RequestHandler):
+    def post(self):
+	user = self.request.get('user')
+	guest = self.request.get('guest')
+	if user:
+	    greeting = Greeting_person(parent = ndb.Key(Webusers, str(user)))
+	    if guest:
+		guest_query = ndb.Key(Webusers,str(guest)).get()
+		if guest_query:
+	            greeting.author = guest_query.nickname
+	    greeting.content = self.request.get('content')
+	    greeting.put()
+	    self.redirect('/person?user=%s' %user)
+ 
+class Activity_comment_api(webapp2.RequestHandler):
+    def post(self):
+	requests = json.loads(self.request.body)
+	activity_id = requests['activity_id']
+	guest_id = requests['guest_id']
+	comment = requests['comment']
+	author = ''
+	activity = ndb.Key(Activity, long(activity_id)).get()
+	if activity:
+	    comment_entity = Activity_comment(parent = ndb.Key(Activity, long(activity_id)))
+	    if guest_id:
+	        guest_query = ndb.Key(Webusers, str(guest_id)).get()
+	    	if guest_query:
+		    comment_entity.author = guest_query.nickname
+		    author = guest_query.nickname
+	    comment_entity.content = comment
+	    comment_entity.put()
+	responses = {
+	    'author':author,
+	    'content':comment
+	}
+        self.response.headers['Content-Type'] = "application/json"
+        self.response.headers['Accept'] = "text/plain"
+        self.response.write(json.dumps(responses))
+ 
+class Subscribe_api(webapp2.RequestHandler):
+    def post(self):
+	user_id = self.request.get('user')
+	guest_id = self.request.get('guest')
+	guest = ndb.Key(Webusers, str(guest_id)).get()
+	guest.subscribe.append(str(user_id))
+	guest.put()	
+	self.redirect('/person?user=%s' %user_id)
+	   
+class Activity_api(webapp2.RequestHandler):
+    def post(self):
+	requests = json.loads(self.request.body)
+	activity_id = requests['activity_id']
+	guest_id = requests['guest_id']
+	activity = ndb.Key(Activity, long(activity_id)).get()
+	if activity:
+	    start_date = str(activity.start_date.year)+'/'+str(activity.start_date.month)+'/'+str(activity.start_date.day)+ ' '+ str(activity.start_date.hour)+':'+str(activity.start_date.minute)
+	    end_date = str(activity.end_date.year)+'/'+str(activity.end_date.month)+'/'+str(activity.end_date.day)+ ' '+ str(activity.end_date.hour)+':'+str(activity.end_date.minute)
+	    date = str(activity.date.year)+'/'+str(activity.date.month)+'/'+str(activity.date.day)+ ' '+ str(activity.date.hour)+':'+str(activity.date.minute)
+	    host = ndb.Key(Webusers, str(activity.host)).get()
+	    host_photo = ''
+	    host_nick = ''
+	    take_action = 1
+	    like_action = 1
+	    guest = ndb.Key(Webusers, str(guest_id)).get()
+	    if guest:
+	       if activity_id in guest.take_activity:
+	           take_action = 0
+	       if activity_id in guest.like_activity:
+	           like_action = 0
+	    all_date = list()
+	    if activity.end_date:
+		start = datetime(activity.start_date.year,activity.start_date.month,activity.start_date.day)
+		end = datetime(activity.end_date.year, activity.end_date.month, activity.end_date.day)
+		delta = end - start
+		for i in range(delta.days+1):
+		    item = start + timedelta(days = i)
+		    all_date.append(str(item.year)+'/'+str(item.month)+'/'+str(item.day))
+	    if host:
+		host_photo = host.photo
+		host_nick = host.nickname
+
+	    responses = {
+		'title':activity.title,
+		'start_date':start_date,
+		'end_date':end_date,
+		'neighborhood':activity.neighborhood,
+		'zipcode':activity.zipcode,
+		'address':activity.address,
+		'latitude':activity.latitude,
+		'longitude':activity.longitude,
+		'host':activity.host,
+		'tag':activity.tag,
+		'details':activity.details,
+		'like_number':activity.like_number,
+		'take_number':activity.take_number,
+		'cover':activity.cover,
+		'date':date,
+		'host_photo':host_photo,
+		'host_nick':host_nick,
+		'all_date':all_date,
+		'like_action':like_action,
+		'take_action':take_action,
+	    }	
+            self.response.headers['Content-Type'] = "application/json"
+            self.response.headers['Accept'] = "text/plain"
+            self.response.write(json.dumps(responses))
+	   
+class Like_api(webapp2.RequestHandler):
+    def post(self):
+	requests = json.loads(self.request.body)
+	activity_id = requests['activity_id']
+        guest_id = requests['guest_id']
+	action = requests['action']
+	activity = ndb.Key(Activity, long(activity_id)).get()
+	guest = ndb.Key(Webusers, str(guest_id)).get()
+	like_number = activity.like_number
+	if action == "1" and (str(activity_id) not in guest.like_activity):
+	    like_number = activity.like_number + 1
+	    activity.like_number = activity.like_number + 1
+	    activity.put()
+	    guest.like_activity.append(str(activity_id))
+	    guest.put()
+	    action = "0"
+	elif action == "0" and (str(activity_id) in guest.like_activity):
+	    like_number = activity.like_number - 1
+	    activity.like_number = activity.like_number - 1
+	    activity.put()
+	    guest.like_activity.remove(str(activity_id))
+	    guest.put()
+	    action = "1" 
+	responses = {
+	    'like':str(like_number),
+	    'action':action
+	}
+        self.response.headers['Content-Type'] = "application/json"
+        self.response.headers['Accept'] = "text/plain"
+        self.response.write(json.dumps(responses))
+
+class Take_api(webapp2.RequestHandler):
+    def post(self):
+	requests = json.loads(self.request.body)
+	activity_id = requests['activity_id']
+	guest_id = requests['guest_id']
+	action = requests['action']
+	comment = requests['comment']
+	activity = ndb.Key(Activity, long(activity_id)).get()
+	if activity:
+	    comment_entity = Activity_comment(parent = ndb.Key(Activity, long(activity_id)))
+	    if guest_id:
+	        guest_query = ndb.Key(Webusers, str(guest_id)).get()
+	    	if guest_query:
+		    comment_entity.author = guest_query.nickname
+	    comment_entity.content = comment
+	    comment_entity.put()
+	guest = ndb.Key(Webusers, str(guest_id)).get()
+	take_number = activity.like_number
+	if action == "1" and (str(activity_id) not in guest.take_activity):
+	    take_number = activity.take_number + 1
+	    activity.take_number = activity.take_number + 1
+	    activity.put()
+	    guest.take_activity.append(str(activity_id))
+	    guest.put()
+	    action = "0"
+	elif action == "0" and (str(activity_id) in guest.take_activity):
+	    take_number = activity.take_number - 1
+	    activity.take_number = activity.take_number - 1
+	    activity.put()
+	    guest.take_activity.remove(str(activity_id))
+	    guest.put()
+	    action = "1"
+	responses = {
+	    'take': str(take_number),
+	    'action': action
+	}
+	print "take number= " + str(take_number)
+        self.response.headers['Content-Type'] = "application/json"
+        self.response.headers['Accept'] = "text/plain"
+        self.response.write(json.dumps(responses))
+    
+application = webapp2.WSGIApplication([
+    ('/api/post',Upload_api),
+    ('/api/profile',Profile_api),
+    ('/api/person',Person_api),
+    ('/api/greeting_person', Greeting_person_api),
+    ('/api/subscribe', Subscribe_api),
+    ('/api/activity', Activity_api),
+    ('/api/like', Like_api),
+    ('/api/take', Take_api),
+    ('/api/activity_comment',Activity_comment_api)
+], debug=True)
---
+++ ui.py
@@ -0,0 +1,343 @@
+import cgi
+import urllib
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.api import files, images
+from google.appengine.api import mail
+from datetime import *
+from api import *
+import webapp2
+import json
+import httplib
+import urllib
+import api
+import mimetypes
+import base64
+import os
+import jinja2
+import re
+import random
+import urllib2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class Index(webapp2.RequestHandler):
+    def get(self):
+	user = users.get_current_user()
+	if user:
+            user_query = ndb.Key(Webusers,user.email()).get()
+	    if not user_query:
+	        self.redirect('/profile_create')
+	    else:
+                logout_url = users.create_logout_url('/')
+	        template_value = {
+	    	'user': user,
+	    	'nickname':user_query.nickname,
+		'logout_url':logout_url 
+	        }
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_value))
+	else:
+	    login_url = users.create_login_url('/')
+	    template_value = {
+		'user': user,
+		'login_url':login_url
+	    }
+	    template = JINJA_ENVIRONMENT.get_template('index.html')
+	    self.response.write(template.render(template_value))
+	
+
+class Post_activity(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+	if not user:
+	    self.redirect(users.create_login_url('/post'))
+	else:
+	    upload_url = blobstore.create_upload_url('/api/post')
+	    print upload_url
+	    print user.email()
+	    template_value = {
+	        'upload_url':upload_url,
+		'user': user.email()
+	    }
+	    template = JINJA_ENVIRONMENT.get_template('post.html')
+	    self.response.write(template.render(template_value))
+
+class Profile_create(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+	if not user:
+	    self.redirect(users.create_login_url('/profile_create'))
+	else:
+	    user_query = ndb.Key(Webusers, str(user.email())).get()
+	    if not user_query:
+	        profile_url = blobstore.create_upload_url('/api/profile')
+	        template_value = {
+	            'profile_url': profile_url,
+	            'user': user.email() 
+	        }
+	    	template = JINJA_ENVIRONMENT.get_template('profile_create.html')
+	        self.response.write(template.render(template_value))
+	    else:
+		self.redirect('/my_city')
+	#else
+class Profile_edit(webapp2.RequestHandler):
+    def get(self):
+	user = users.get_current_user()
+	if not user:
+	    self.redirect(users.create_login_url('/profile_edit'))
+	else:
+	    user_query = ndb.Key(Webusers, str(user.email())).get()
+	    if not user_query:
+		self.redirect('/profile_create')
+	    else:
+		profile_url = blobstore.create_upload_url('/api/profile')
+		if user_query.birthday:
+		    birthday = str(user_query.birthday.year) + '/'+str(user_query.birthday.month) + '/' + str(user_query.birthday.day)
+		else:
+		    birthday = ''
+		template_value = {
+		    'user':user.email(),
+		    'profile_url':profile_url,
+		    'nick':user_query.nickname,
+		    'gender':user_query.gender,
+		    'birthday':birthday,
+		    'introduce':user_query.introduce,
+		    'interest':user_query.interest
+		}
+	    	template = JINJA_ENVIRONMENT.get_template('profile_edit.html')
+	        self.response.write(template.render(template_value))
+#My city
+class My_city(webapp2.RequestHandler):
+    def get(self):
+	user = users.get_current_user()
+	if not user:
+	    self.redirect(users.create_login_url('/my_city'))
+	else:
+	    requests = {
+		'user_id':str(user.email())
+	    }
+            headers = {"Content-type": "application/json", "Accept": "text/plain"}
+            conn = httplib.HTTPConnection("localhost","8888")
+            conn.request("POST", "/api/person", json.dumps(requests), headers)
+            responses = conn.getresponse()
+	    #Get date
+	    nick = ''
+	    take_activity = list()
+	    like_activity = list()
+	    post_activity = list()
+	    introduce = ''
+	    photo = ''
+	    gender = ''
+	    interest_tag = list()
+	    sub_me_number = 0
+	    greetings_author=list()
+	    greetings_content = list()
+            login_url = users.create_login_url(self.request.uri)
+            if responses.status == 200:
+		data = json.loads(responses.read())
+		photo = data['photo']
+		taken = data['taken']
+		like = data['like']
+		post = data['post']
+		nick = data['nick']
+		gender = data['gender']
+		introduce = data['introduce']
+		interest = data['interest']
+		sub_me_number = data['sub_me_number']
+	        greetings_author = data['greetings_author']
+	        greetings_content = data['greetings_content']
+		greetings = Greeting_person.query(ancestor=ndb.Key(Webusers, str(user.email()))).order(Greeting_person.date).fetch()
+		for activity in taken:
+		    take_activity.append(ndb.Key(Activity, long(activity)).get())
+		for activity in like:
+		    like_activity.append(ndb.Key(Activity, long(activity)).get())
+		for activity in post:
+		    post_activity.append(ndb.Key(Activity, long(activity)).get())
+		for tag in interest:
+		    interest_tag.append(str(tag))
+	    template_value = {
+		'nick': nick,
+		'take_activity':take_activity,
+		'like_activity':like_activity,
+		'post_activity':post_activity,
+		'introduce':introduce,
+		'interest':interest_tag,
+		'sub_me_number':sub_me_number,
+		'len_interest':len(interest_tag),
+		'gender':gender,
+		'photo':photo,
+		'greetings_author':greetings_author,
+		'greetings_content':greetings_content,
+		'greetings_number':len(greetings_author),
+		'user': user.email(),
+		'guest': user.email(),
+		'greetings':greetings,
+		'login_url':login_url
+	    }
+	    template = JINJA_ENVIRONMENT.get_template('my_city.html')
+	    self.response.write(template.render(template_value))
+	
+class Image_Show(blobstore_handlers.BlobstoreDownloadHandler):
+   def get(self):
+       blob_key = self.request.get('key')
+       blob_info = blobstore.BlobInfo.get(str(urllib.unquote(blob_key)))
+       self.send_blob(blob_info)
+
+class Person(webapp2.RequestHandler):
+    def get(self):
+	user = self.request.get('user')
+	guest = users.get_current_user()
+	guest_id = ''
+	flag = 0
+	if guest: 
+	    if user == guest.email():
+		flag = 1
+	        self.redirect('/my_city')
+	    guest_id = guest.email()
+	if flag == 0:
+	    requests = {
+		'user_id':str(user)
+	    }
+            headers = {"Content-type": "application/json", "Accept": "text/plain"}
+            conn = httplib.HTTPConnection("localhost","8888")
+            conn.request("POST", "/api/person", json.dumps(requests), headers)
+            responses = conn.getresponse()
+	    #Get date
+	    nick = ''
+	    take_activity = list()
+	    like_activity = list()
+	    post_activity = list()
+	    introduce = ''
+	    photo = ''
+	    gender = ''
+	    interest_tag = list()
+	    greetings_author=list()
+	    greetings_content = list()
+	    sub_me_number = 0
+            if responses.status == 200:
+		data = json.loads(responses.read())
+		photo = data['photo']
+		taken = data['taken']
+		like = data['like']
+		post = data['post']
+		nick = data['nick']
+		gender = data['gender']
+		introduce = data['introduce']
+		interest = data['interest']
+		sub_me_number = data['sub_me_number']
+		for activity in taken:
+		    take_activity.append(ndb.Key(Activity, long(activity)).get())
+		for activity in like:
+		    like_activity.append(ndb.Key(Activity, long(activity)).get())
+		for activity in post:
+		    post_activity.append(ndb.Key(Activity, long(activity)).get())
+		for tag in interest:
+		    interest_tag.append(str(tag))
+	        greetings_author = data['greetings_author']
+	        greetings_content = data['greetings_content']
+		greetings = Greeting_person.query(ancestor=ndb.Key(Webusers, str(user))).order(Greeting_person.date).fetch()
+	    template_value = {
+		'nick': nick,
+		'take_activity':take_activity,
+		'like_activity':like_activity,
+		'post_activity':post_activity,
+		'introduce':introduce,
+		'interest':interest_tag,
+		'sub_me_number':sub_me_number,
+		'len_interest':len(interest_tag),
+		'gender':gender,
+		'photo':photo,
+		'greetings_author':greetings_author,
+		'greetings_content':greetings_content,
+		'greetings_number':len(greetings_author),
+		'user': user,
+		'guest': guest_id,
+		'greetings':greetings
+	    }
+	    template = JINJA_ENVIRONMENT.get_template('person.html')
+	    self.response.write(template.render(template_value))
+
+class Activity_page(webapp2.RequestHandler):
+    def get(self):	    
+	guest_id = ''
+	user = users.get_current_user()
+	if user:
+	    guest_id = user.email()
+	activity_id = str(self.request.get('id'))
+        requests = {
+	'activity_id':activity_id,
+	'guest_id': guest_id
+	}
+	comments = Activity_comment.query(ancestor=ndb.Key(Activity, long(activity_id))).order(Activity_comment.date).fetch()
+        headers = {"Content-type": "application/json", "Accept": "text/plain"}
+        conn = httplib.HTTPConnection("localhost","8888")
+        conn.request("POST", "/api/activity", json.dumps(requests), headers)
+        responses = conn.getresponse()
+        if responses.status == 200:
+	    data = json.loads(responses.read())
+	    print "host" + data['host_photo']
+	    template_value = {
+	    	'title': data['title'],
+		'start_date':data['start_date'],
+		'end_date':data['end_date'],
+		'neighborhood':data['neighborhood'],
+		'zipcode':data['zipcode'],
+		'address':data['address'],
+		'latitude':data['latitude'],
+		'longitude':data['longitude'],
+		'host':data['host'],
+		'tag':data['tag'],
+		'details':data['details'],
+		'like_number':data['like_number'],
+		'take_number':data['take_number'],
+		'cover':data['cover'],
+		'date':data['date'],
+		'host_photo':data['host_photo'],
+		'host_nick':data['host_nick'],
+		'activity_id':str(self.request.get('id')),
+		'guest': guest_id,
+		'like_action':data['like_action'],
+		'take_action':data['take_action'],
+		'all_date':data['all_date'],
+		'more':len(data['all_date'])-1,
+		'comments':comments
+	    }
+	    print "like_action:" + str(data['like_action'])
+	    template = JINJA_ENVIRONMENT.get_template('activity.html')
+	    self.response.write(template.render(template_value))
+	    
+	    
+		
+
+application = webapp2.WSGIApplication([
+	('/post', Post_activity),
+	('/profile_create', Profile_create),
+	('/profile_edit', Profile_edit),
+	('/my_city', My_city),
+	('/', Index),
+	('/img', Image_Show),
+	('/person', Person),
+	('/activity',Activity_page),
+], debug = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
